e942c3c4efca7291b9460003d6bf3373280ef751
jose20pavas <jomapago05@gmail.com>
2024-02-18 15:33:25 -0500

    commit 1

---
+++ p1.py
@@ -0,0 +1,3 @@
+Nombre = str(input("ingrese su nombre: "))
+Edad = int(input("ingrese su edad: "))
+print("Hola ", Nombre, "su edad es", Edad)
---
+++ p2.py
@@ -0,0 +1,5 @@
+# Escribir una función que calcule el área de un círculo dado su radio.
+
+diametro = int(input("ingrese el diamtro del circulo: "))
+radio = diametro / 2
+print("su radio es: ", radio)
